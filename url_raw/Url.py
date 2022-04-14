e3b1b8b60a6dbf29213c18e0a5fce87e88bb02f9
lethehoa <tretrauchamcom1@gmail.com>
2022-04-14 18:55:07 +0700

    draft2

--- url_raw/Url.py
+++ url_raw/Url.py
@@ -16,13 +16,14 @@ class Url:
     def received_From_User(self):
         test = TemplateUtil.TemplateUtil() 
         url_from_user = test.readConfigFile()['url'] #Hiện tại không có bên TemplateUtil - bổ sung sau (update 12h30 - 14/04)
-        print(type(url_from_user))
         test1 = TemplateUtil.TemplateUtil().readRequestTemplate()
         print(test1[0])
         # request_from_template = TemplateUtil.readRequestTemplate()
         for req in test1:
-            Util.Util().findAndReplaceURLPattern(req, '{{Hostname}}', test)
-
+            Util.Util().findAndReplaceURLPattern(req, '{{Hostname}}', url_from_user)
+            print(req)
+        # print("\nAfter")
+        # print(test1[0])
 
 test2 = Url()
 test2.received_From_User()
