130ca0f55b1a410e573d050662f61745c6bbaff7
Dat Tran <68977796+tranduydat2107@users.noreply.github.com>
2022-05-20 11:32:50 +0700

    refactor package scanner to services

--- Raccoon.py
+++ Raccoon.py
@@ -1,13 +1,13 @@
-from scanner.CommandHandle import CommandUtil
+from services.CommandHandle import CommandUtil
 from generator.TemplateConfigGenerator import TemplateConfigService
-from scanner.RaccoonKernel import RaccoonKernel
+from services.RaccoonKernel import RaccoonKernel
 from generator.RequestGenerator import RequestGenerator
 import urllib3
 from config.StaticData import Template
 from utils.ConfigUtil import ConfigUtil
 from config.StaticData import HTMLReportGlobal
 from utils.FileUtil import FileUtil
-from scanner.Debugger import Debugger
+from services.Debugger import Debugger
 from config.StaticData import Debug
 
 
